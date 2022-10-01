e7bea4a63a7bc63489765f1fe3373b7c13e5c509
leohuet <104456230+leohuet@users.noreply.github.com>
2022-10-01 16:27:53 +0200

    Add files via upload

---
+++ blog_code/instagram_graph_api/index.php
@@ -0,0 +1,15 @@
+<?php
+  require_once('instagram_basic_display_api');
+
+  $params = array(
+    'get_code' => isset($_GET['code']) ? $_GET['code'] : ''
+  );
+  $ig = new instagram_basic_display_api($params);
+?>
+
+<h1>Instagram Basic Display API</h1>
+<hr />
+
+<a href="<?php $ig->authorizationUrl; ?>">
+  Authorize w/Instagram
+</a>
