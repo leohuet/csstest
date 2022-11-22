0bf231cf8639b062cecba27852a6d864ef67b2e9
leohuet <104456230+leohuet@users.noreply.github.com>
2022-11-22 11:20:27 +0100

    Delete blog_code/instagram_graph_api directory

--- blog_code/instagram_graph_api/defines.php
+++
@@ -1,4 +0,0 @@
-<?php
-  define('INSTAGRAM_APP_ID', '1226109811544326');
-  define('INSTAGRAM_APP_SECRET', 'fb24edea046fa73689f0c206d82ba662');
-  define('INSTAGRAM_APP_REDIRECT_URI', 'https://leohuet.github.io/apitest.github.io/blog_code/instagram_graph_api/');
